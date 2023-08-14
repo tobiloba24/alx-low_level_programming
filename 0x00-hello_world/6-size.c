90515bf39021d81077188348839f6078cc67b373
Ariyo <tabgoal@gmail.com>
2023-08-14 11:40:38 -0700

    size of types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main- printing various types in the computer
+ *
+ * Return: 0 when succesful
+ */
+
+int main(void)
+{
+	printf"size of char: %lu byte(s)", sizeof(char));
+	printf"size of int: %lu byte(s)", sizeof(int));
+	printf"sizof long int: %lu byte(s)", sizeof(long int));
+	printf"size  long long int: %lu byte(s)", sizeof(long long int));
+	printf"size of float: %lu byte(s)", sizeof(float));
+	return (0);
+
+}
