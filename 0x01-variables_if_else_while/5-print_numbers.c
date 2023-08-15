f98860d6e37ec35a61cec09be6fadbfe085b93d2
Ariyo <tabgoal@gmail.com>
2023-08-14 21:12:26 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main- entry point
+ *
+ * Return: should be 0 when succesful
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
+
