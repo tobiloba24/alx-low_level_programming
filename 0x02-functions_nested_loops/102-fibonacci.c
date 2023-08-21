8691b6f8f7f38ef84d2ec7796c3fc6e329423e2d
Ariyo <tabgoal@gmail.com>
2023-08-21 05:30:37 -0700

    computer class

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,47 +1,29 @@
 #include <stdio.h>
 
 /**
- * main - fibonacci <3
+ * main - function to find first 50 fibonnacci number
  *
- * Purpose - no hardcode
- *
- * Return:  (Success)
+ * Return: 0
  */
 
 int main(void)
 {
-	unsigned long int i;
-	unsigned long int bef = 1;
-	unsigned long int aft = 2;
-	unsigned long int l = 1000000000;
-	unsigned long int bef1;
-	unsigned long int bef2;
-	unsigned long int aft1;
-	unsigned long int aft2;
+	int count;
+	unsigned long fib1 = 0, fib2 = 1, sum;
 
-	printf("%lu", bef);
 
-	for (i = 1; i < 91; i++)
+	for (count = 0; count < 50; count++)
 	{
-		printf(", %lu", aft);
-		aft += bef;
-		bef = aft - bef;
-	}
+		sum = fib1 + fib2;
+		printf("%lu", sum);
 
-	bef1 = (bef / l);
-	bef2 = (bef % l);
-	aft1 = (aft / l);
-	aft2 = (aft % l);
+		fib1 = fib2;
+		fib2 = sum;
 
-	for (i = 92; i < 99; ++i)
-	{
-		printf(", %lu", aft1 + (aft2 / l));
-		printf("%lu", aft2 % l);
-		aft1 = aft1 + bef1;
-		bef1 = aft1 - bef1;
-		aft2 = aft2 + bef2;
-		bef2 = aft2 - bef2;
-	}
-	printf("\n");
+		if (count == 49)
+			printf("\n");
+		else
+			printf(", ");
+		}
 	return (0);
 }
