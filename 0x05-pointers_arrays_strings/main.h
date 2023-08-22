36bc236cf33b9fc414429aff938a53796dd5a2f1
Ariyo <tabgoal@gmail.com>
2023-08-22 12:03:49 -0700

    arrays are not pointers

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
 
 int _putchar(char c);
 void reset_to_98(int *n);
@@ -10,6 +11,7 @@ void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
+void print_array(int *a, int n);
 
 
 #endif
