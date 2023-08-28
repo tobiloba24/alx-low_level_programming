c5d0a33301365b08139a14c503dfafef98ef7fbd
Ariyo <tabgoal@gmail.com>
2023-08-28 10:06:08 -0700

    mesmet

---
+++ 0x07-pointers_arrays_string/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memset - fills of memory with specific value
+ * @s: the starting address of memory to be filled
+ * @b: desired value
+ * @n: number of bytes to be changed
+ *
+ * Return: change array with new value for n bytes
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	for (; n > 0; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+	return (s);
+}
