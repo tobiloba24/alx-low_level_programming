afa47498d9538227e1935a0898f0968f605ddbc5
Ariyo <tabgoal@gmail.com>
2023-09-01 06:34:13 -0700

    all argument

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints all the arguments it receives
+ * @argc: arguments count
+ * @argv: argument vector
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
