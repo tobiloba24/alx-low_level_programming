78935397cff58e64dd9577f4dd367b66e0a25b5b
Ariyo <tabgoal@gmail.com>
2023-09-26 12:47:15 -0700

    print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - prits the elements of linked list
+ * @h: the pointer to the list_t list
+ *
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t s = 0;
+
+	while (h)
+	{
+	if (!h->str)
+	printf("[0] (nil)\n");
+
+	else
+	printf("[%u] %s\n", h->len, h->str);
+	h = h->next;
+
+	s++;
+	}
+	return (s);
+}
