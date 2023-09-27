c2235adf71274938dba1a4846e9c5bcea77599e4
Ariyo <tabgoal@gmail.com>
2023-09-26 20:05:03 -0700

    add node

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - this adds a new node at the begnning of a linkled list
+ * @head: pointer to the first node
+ * @n: data to insert 
+ *
+ * Return: pointer to new node or NULL
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
+
+	new->n = n;
+	new->next = *head;
+	*head = new;
+
+	return (new);
+}
